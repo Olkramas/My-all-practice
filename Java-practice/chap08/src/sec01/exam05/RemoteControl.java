5560d928622fbe97dc025855dae1a3baed725a9c
CHOI TAEBAEK <96291117+Olkramas@users.noreply.github.com>
2024-09-04 22:57:52 +0900

    Add files via upload

---
+++ Java-practice/chap08/src/sec01/exam05/RemoteControl.java
@@ -0,0 +1,12 @@
+package sec01.exam05;
+
+public interface RemoteControl {
+	int MAX_VOLUME = 10;
+	int MIN_VOLUME = 0;
+	
+	//Method
+	//public abstract 생략됨, 실행문도 적지 않음{}내용 말하는거
+	void turnOn();
+	void turnOff();
+	void setVolume(int volume);
+}
---
+++ Java-practice/chap08/src/sec02/exam02/Driver.java
@@ -0,0 +1,7 @@
+package sec02.exam02;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run(); 						//vehicle에 대입된 구현객체의 .run이 실행된다.
+	}
+}
