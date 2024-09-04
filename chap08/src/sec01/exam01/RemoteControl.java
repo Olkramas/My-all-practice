001b40db246d87e65b6edc39a1e8bb77f8d02bf7
CHOI TAEBAEK <96291117+Olkramas@users.noreply.github.com>
2024-09-04 22:32:32 +0900

    Delete chap08 directory

--- chap08/src/sec01/exam01/RemoteControl.java
+++
@@ -1,9 +0,0 @@
-package sec01.exam01;
-
-public interface RemoteControl {
-	//상수
-	//public static final 생략 가능, 인터페이스라 그 특성을 그대로 가짐
-	int MAX_VOLUME = 10;
-	int MIN_VOLUME = 0;
-	
-}
--- chap08/src/sec02/exam02/Driver.java
+++
@@ -1,7 +0,0 @@
-package sec02.exam02;
-
-public class Driver {
-	public void drive(Vehicle vehicle) {
-		vehicle.run(); 						//vehicle에 대입된 구현객체의 .run이 실행된다.
-	}
-}
